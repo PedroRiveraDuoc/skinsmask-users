5ccfb9a439af95046c61ad385f885ccfcc81c444
PedroRiveraDuoc <p.riveral@duocuc.cl>
2024-11-29 18:00:43 -0300

    imports clean

--- src/main/java/com/duocuc/backend_srv/security/CustomUserDetailsService.java
+++ src/main/java/com/duocuc/backend_srv/security/CustomUserDetailsService.java
@@ -1,10 +1,9 @@
 package com.duocuc.backend_srv.security;
 
-import java.util.Set;
+
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
